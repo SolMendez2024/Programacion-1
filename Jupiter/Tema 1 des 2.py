6fe3516190e5bfea4a90b3ba64f6d11d942709da
SolMendez2024 <bau21062014@gmail.com>
2024-10-08 11:41:22 -0300

    8/10

---
+++ Jupiter/Tema 1 des 2.py
@@ -0,0 +1,17 @@
+"""
+Grupo: Programación
+Creador:bau21062014
+Colaboradores:
+Fecha:03-04-24
+"""
+
+#Definir Variable Ancho 5
+Ancho= 5
+
+#Definir Variable Largo 8
+Largo= 8
+
+#Calcular el area del rectangulo multiplicando Largo por Ancho
+Area= Largo*Ancho
+
+#Muestro la variable del resultado de forma amigable
