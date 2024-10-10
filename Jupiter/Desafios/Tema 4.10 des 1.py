b01aa8a330263bcde5b8b1709d55cd7bb6b8467b
SolMendez2024 <bau21062014@gmail.com>
2024-10-10 12:26:46 -0300

    10/10

--- Jupiter/Desafios/Tema 4.10 des 1.py
+++ Jupiter/Desafios/Tema 4.10 des 1.py
@@ -1,26 +1,24 @@
 
 """Desafío 1: Sistemas con Múltiples Entidades Interconectadas (Biblioteca)
-Vamos a simplificar el código usando listas y clases para manejar 
-la interacción entre libros y usuarios en la biblioteca.
+Imagina un sistema de gestión de biblioteca que maneja libros, 
+usuarios, préstamos y multas. 
+Usar TADs separados  para cada uno de estos elementos 
+podría complicar la  interacción y gestión de relaciones entre ellos.
 Materia:Programación 1
 Alumno: Sol Méndez
 Fecha: 07-10-2024"""
 
-
-#Definir un libro
-class libro:
-    def __init__(self, titulo):
-        self.libro = libro 
-
-#Definir usuario
-class usuario:
-     def __init__(self, nombre):
-         self.nombre = nombre
-         self.libro_prestados= [] #Lista de libros prestados
-
-     def prestar_libro(self, libro) #Agregar libro a la lista
-
-#Crear lista
+# Listas para almacenar los datos
+libros = []
+usuarios = []
+prestamos = []
+multas = []
 
 
-         
+# Definir la lista de libros
+
+def libros(titulo, autor, año):
+    
+
+    
+    
