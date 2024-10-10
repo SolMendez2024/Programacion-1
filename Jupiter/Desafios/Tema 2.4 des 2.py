d52ec4c3a58986f8ab55a979f6354b233e91fff6
SolMendez2024 <bau21062014@gmail.com>
2024-10-10 11:19:52 -0300

    10/10

---
+++ Jupiter/Desafios/Tema 2.4 des 2.py
@@ -0,0 +1,20 @@
+
+"""Grupo: Programación 
+Consigna:Desafío 2: Calcular promedio. Crear un programa que lea 5 calificaciones y calcule su promedio
+Creador:bau21062014 
+Fecha:03-04-24"""
+
+
+
+# Definir las 5 calificaciones directamente
+calificacion1 = 8.5
+calificacion2 = 7.0
+calificacion3 = 9.2
+calificacion4 = 6.8
+calificacion5 = 8.0
+
+# Calcular el promedio
+promedio = (calificacion1 + calificacion2 + calificacion3 + calificacion4 + calificacion5) / 5
+
+# Mostrar el resultado
+print("El promedio de las calificaciones es:", promedio)
---
+++ Jupiter/Desafios/Tema 4.10 des 1.py
@@ -0,0 +1,26 @@
+
+"""Desafío 1: Sistemas con Múltiples Entidades Interconectadas (Biblioteca)
+Vamos a simplificar el código usando listas y clases para manejar 
+la interacción entre libros y usuarios en la biblioteca.
+Materia:Programación 1
+Alumno: Sol Méndez
+Fecha: 07-10-2024"""
+
+
+#Definir un libro
+class libro:
+    def __init__(self, titulo):
+        self.libro = libro 
+
+#Definir usuario
+class usuario:
+     def __init__(self, nombre):
+         self.nombre = nombre
+         self.libro_prestados= [] #Lista de libros prestados
+
+     def prestar_libro(self, libro) #Agregar libro a la lista
+
+#Crear lista
+
+
+         
