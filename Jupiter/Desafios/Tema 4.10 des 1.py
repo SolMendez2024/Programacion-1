44f7cc097bc7ebba88bec2062ce7f353fcd0f89c
SolMendez2024 <bau21062014@gmail.com>
2024-10-10 12:48:36 -0300

    10/10

--- Jupiter/Desafios/Tema 4.10 des 1.py
+++ Jupiter/Desafios/Tema 4.10 des 1.py
@@ -9,6 +9,7 @@ Alumno: Sol Méndez
 Fecha: 07-10-2024"""
 
 # Listas para almacenar los datos
+
 libros = []
 usuarios = []
 prestamos = []
@@ -17,8 +18,39 @@ multas = []
 
 # Definir la lista de libros
 
-def libros(titulo, autor, año):
-    
+def agregar_libros (titulo, autor, año): 
+    libro = { "titulo": titulo, "autor": autor, "año": año}
+    libros.append(libro)
+    print(f"Libro'{titulo}'agregando correctamente")
 
-    
+#Definir la lista de usuarios
+def agregar_usuario(nombre, apellido, edad):
+    usuario = { "nombre": nombre, "apellido": apellido, "edad": edad}
+    usuarios.append(usuario)
+    print(f"Usuario '{nombre} {apellido}' agregado correctamente")
+
+#Definir la lista de prestamo 
+def agregar_prestamo(libro, fecha, usuario, devolucion):
+    prestamo = { "libro": libro, "fecha": fecha, "usuario": usuario, "devolucion": devolucion}
+    prestamos.append(prestamo)
+    print(f"Prestamo de libro '{libro['titulo']}' para el usuario '{usuario['nombre']} {usuario['apellido']}' agregado correctamente")
+
+#Definir la lista de multas
+def agregar_multa(prestamo, monto, usuario):
+    multa = { "prestamo": prestamo, "monto": monto, "usuario": usuario}
+    multas.append(multa)
+    print(f"Multa de monto '{monto}' agregada correctamente para el usuario '{usuario['nombre']} {usuario['apellido']}'") 
+
+#Ejemplos de uso
+
+agregar_libros ("El Quijote", "Miguel de Cervantes", 1605)
+agregar_libros ("La Divina Comedia", "J.B.R. Tolkien", 1937)
+
+agregar_usuario= ("Juan", "Perez", 25)
+agregar_usuario= ("Maria", "Garcia", 30)
+
+agregar_prestamo (libros[0], "01-05-2024", usuarios[0], "05-10-2024")
+
+agregar_multa (prestamos[0], 10, usuarios[0])
+
     
